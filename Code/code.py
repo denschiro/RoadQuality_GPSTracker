7e0d90b0a11d2d770a614574c2c9a19de987b579
denschiro <bradtmoeller@googlemail.com>
2022-06-19 07:02:17 +0200

    change time and gps data generate

--- Code/code.py
+++ Code/code.py
@@ -64,7 +64,7 @@ import digitalio  # for led,piezo
 import gnss  # for gps
 import busio  # for i2c
 import adafruit_mpu6050  # for mpu6050
-import adafruit_sdcard  # better SD Card use
+import adafruit_sdcard # better SD Card use
 
 # define
 led_sd = digitalio.DigitalInOut(board.LED0)
@@ -83,7 +83,6 @@ mpu.gyro_range = adafruit_mpu6050.GyroRange.RANGE_250_DPS
 
 
 nav = gnss.GNSS([gnss.SatelliteSystem.GPS, gnss.SatelliteSystem.GLONASS])
-
 
 def mount_sd():
 
@@ -115,18 +114,28 @@ def write_data(data, file_name="track.txt"):
         f.write(str(data) + "\r\n")
 
 
+
 def get_gps():
     if nav.fix is gnss.PositionFix.INVALID:
+        led_gps.value = False
         return "Waiting for fix..."
     else:
-        t = nav.timestamp[:6]
-        lat = nav.latitude
-        lon = nav.longitude
-        alt = nav.altitude
-        print("Latitude: {0:.6f} degrees".format(nav.latitude))
-        print("Longitude: {0:.6f} degrees".format(nav.longitude))
-        print("Altitude {0:.6f} meters".format(nav.altitude))
-        print("Time: {} ".format(nav.timestamp[:6]))
+        if led_gps.value is not True :
+            led_gps.value = True
+        #t = nav.timestamp[:6]
+        #t_str = ("{},{},{},{}:{}:{},".format(t[0],t[1],t[2],t[3],t[4],t[5]))
+        #lat = str(nav.latitude)
+        #lon = str(nav.longitude)
+        #alt = str(nav.altitude)
+        #print("Latitude: {0:.6f} degrees".format(nav.latitude))
+        #print("Longitude: {0:.6f} degrees".format(nav.longitude))
+        #print("Altitude {0:.6f} meters".format(nav.altitude))
+        #print("Time: {} ".format(nav.timestamp[:6]))
+
+        lat=("Latitude: {0:.6f} ".format(nav.latitude))
+        lon=("Longitude: {0:.6f}".format(nav.longitude))
+        alt=("Altitude: {0:.6f} ".format(nav.altitude))
+        t=("Time: {} ".format(nav.timestamp[:6]))
     return t, lat, lon, alt
 
 
@@ -135,19 +144,19 @@ def mpu_dataframe():
     gyro_sum_value = sum_gyro(tp_mpu[3:])
     print((gyro_sum_value,))
 
-    data_string = ""
+    data_string=""
     for elem in tp_mpu:
-        data_string = data_string + "," + str(elem)
-    data_string += "," + str(gyro_sum_value)
-    # print(data_string)
-    return [data_string, gyro_sum_value]
-
+        data_string = data_string+"," + str(elem)
+    data_string +=  "," + str(gyro_sum_value)
+    #print(data_string)
+    return [data_string , gyro_sum_value]
 
 def piezo_on_off(state=None):
     if state == True:
         piezo.value = True
     else:
         piezo.value = False
+
 
 
 def sum_gyro(tp_gyro):
@@ -158,11 +167,11 @@ def sum_gyro(tp_gyro):
             print("Alert")
             piezo_on_off(True)
 
+
     return sum
 
-
 sd_ready = False
-# check for sd card , blink if a Error
+#check for sd card , blink if a Error
 while sd_ready == False:
     print("SD mount")
     led_sd.value = not led_sd.value
@@ -174,14 +183,14 @@ last_print = time.monotonic()
 max_value = 0.0
 
 while True:
-    # message =  timestamp,latitude,longitude,altitude,acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z,gyro_sum_value,gyro_max_value
+    #message =  timestamp,latitude,longitude,altitude,acc_x,acc_y,acc_z,gyro_x,gyro_y,gyro_z,gyro_sum_value,gyro_max_value
 
     # this prints out all the values like a tuple which Mu's plotter prefer
-    # print("(%.2f, %.2f, %.2f " % (mpu.acceleration), end=", ")
-    # print("%.2f, %.2f, %.2f)" % (mpu.gyro))
+    #print("(%.2f, %.2f, %.2f " % (mpu.acceleration), end=", ")
+    #print("%.2f, %.2f, %.2f)" % (mpu.gyro))
     mpu_data = mpu_dataframe()
 
-    if max_value == 0.0 or float(mpu_data[1]) > max_value:
+    if (max_value == 0.0 or float(mpu_data[1]) > max_value):
         max_value = float(mpu_data[1])
     time.sleep(0.1)
 
@@ -190,10 +199,10 @@ while True:
     if current - last_print >= 1.0:
         last_print = current
         gps_data = get_gps()
-        # if gps_data == "Waiting for fix...":
-        #    pass
+        print(gps_data)
 
-        write_data(gps_data + mpu_data[0] + "," + str(max_value))
-        max_value = 0.0  # reset after write to sd
+
+        write_data(gps_data + mpu_data[0] + "," +str(max_value))
+        max_value = 0.0 #reset after write to sd
         if piezo.value == True:
             piezo.value = False
