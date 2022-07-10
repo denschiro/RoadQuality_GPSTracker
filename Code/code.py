d0ecef64876678f36ac59b9bc4dd5b4d07b78b2e
Daniel Bradtmöller <bradtmoeller@gmail.com>
2022-07-10 22:42:36 +0200

    Remove some old unsued code

--- Code/code.py
+++ Code/code.py
@@ -1,60 +1,3 @@
-"""
-import board
-import digitalio
-import time
-
-led = digitalio.DigitalInOut(board.LED0)
-led.direction = digitalio.Direction.OUTPUT
-
-while True:
-    led.value = True
-    time.sleep(0.5)
-    led.value = False
-    time.sleep(0.5)
-    print("DonE")
-"""
-"""
-import gnss
-import time
-
-nav = gnss.GNSS([gnss.SatelliteSystem.GPS, gnss.SatelliteSystem.GLONASS])
-last_print = time.monotonic()
-while True:
-    nav.update()
-    current = time.monotonic()
-    if current - last_print >= 1.0:
-        last_print = current
-        if nav.fix is gnss.PositionFix.INVALID:
-            print("Waiting for fix...")
-            continue
-        print("Latitude: {0:.6f} degrees".format(nav.latitude))
-        print("Longitude: {0:.6f} degrees".format(nav.longitude))
-        print("Altitude {0:.6f} meters".format(nav.altitude))
-        print("Time: {} ".format(nav.timestamp))
-
-
-"""
-
-"""
-read gps gsnss when True led on else blink
-
-check if sd card is ready then
-check read gps , 3d acc
-write gps data  and 3 acc to  file on sd (get date? frommgps? use as file name)
-(Structure of data as list,dic or json?)[time,latitude,longitude,Acceleration.X,Acceleration.Y,Acceleration.Z,Gyro.X,Gyro.Y,Gyro.Z,temperature]
-
-loop
-
-    read gps , 3d acc
-    if  3d acc extrem  piezo on   (proof for change of acc in two axis , then switch the piezo of )
-    write to sd
-    after sec  start new read
-
-
-
-"""
-
-
 import board
 import storage
 import time
@@ -64,14 +7,12 @@ import digitalio  # for led,piezo
 import gnss  # for gps
 import busio  # for i2c
 import adafruit_mpu6050  # for mpu6050
-import adafruit_sdcard # better SD Card use
 
 # define
 led_sd = digitalio.DigitalInOut(board.LED0)
 led_sd.direction = digitalio.Direction.OUTPUT
 led_gps = digitalio.DigitalInOut(board.LED1)
 led_gps.direction = digitalio.Direction.OUTPUT
-
 
 piezo = digitalio.DigitalInOut(board.D10)
 piezo.direction = digitalio.Direction.OUTPUT
@@ -85,16 +26,13 @@ mpu.gyro_range = adafruit_mpu6050.GyroRange.RANGE_250_DPS
 nav = gnss.GNSS([gnss.SatelliteSystem.GPS, gnss.SatelliteSystem.GLONASS])
 
 def mount_sd():
-
     try:
-
         sd = sdioio.SDCard(
             clock=board.SDIO_CLOCK,
             command=board.SDIO_COMMAND,
             data=board.SDIO_DATA,
             frequency=25000000,
         )
-
         vfs = storage.VfsFat(sd)
         storage.mount(vfs, "/sd")
 
@@ -112,7 +50,6 @@ def write_data(data, file_name="track.txt"):
     with open("/sd/" + file_name, "a") as f:
         print("Write to sd: {}".format(data))
         f.write(str(data) + "\r\n")
-
 
 
 def get_gps():
@@ -172,7 +109,7 @@ def sum_gyro(tp_gyro):
 
 sd_ready = False
 #check for sd card , blink if a Error
-while sd_ready == False:
+while not sd_ready:
     print("SD mount")
     led_sd.value = not led_sd.value
     sd_ready = mount_sd()
@@ -201,8 +138,7 @@ while True:
         gps_data = get_gps()
         print(gps_data)
 
-
         write_data(gps_data[0] + gps_data[1] +"," +gps_data[2] +"," +gps_data[3] + mpu_data[0] + "," +str(max_value))
         max_value = 0.0 #reset after write to sd
-        if piezo.value == True:
+        if piezo.value:
             piezo.value = False
