51e97ce07a3e0ea19b297159462650c65a30a449
Daniel Bradtmöller <bradtmoeller@gmail.com>
2022-08-14 08:45:51 +0200

    Add marker for bad points on the map
    Visible marker on the map for bad points on the track

--- Visu_Track/track_visu.py
+++ Visu_Track/track_visu.py
@@ -1,9 +1,11 @@
 import folium
 
 gps_list = []
+vib_red_list = []
+i = 0
 with open("track1.txt" ,"r" )as file:
     for elem in file:
-        i = 0
+
         #print(elem)
         raw_str = elem.strip()
         raw_list = list(raw_str.split(","))
@@ -15,19 +17,30 @@ with open("track1.txt" ,"r" )as file:
         #gps_data = time_data, lat,lon,alt,vib
         gps_data = (float(lat), float(lon))#,float(vib))
         gps_list.append(gps_data)
+        if float(vib) > 1.3:
+            data = (float(lat), float(lon),str("red"),vib)
+            vib_red_list.append(data)
 
 
-print(gps_list)
+        i +=1
 
 
+#print(gps_list)
 
 m = folium.Map(location=gps_list[0],
               zoom_start=15)
-
+#make route
 loc = gps_list
 folium.PolyLine(loc,
-                color='red',
-                weight=15,
+                color= 'blue',
+                weight=10,
                 opacity=0.8).add_to(m)
 m
+
+for elem in vib_red_list:  #add circle to bad points
+    print(elem)
+    marker = folium.CircleMarker(elem[:2],radius= 4,color=elem[2], popup="Attention" )
+    marker.add_to(m)
+
+
 m.save('route.html')
